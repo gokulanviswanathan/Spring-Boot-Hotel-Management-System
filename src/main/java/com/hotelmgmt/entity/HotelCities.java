5ac675162cb6a9785ee21d0a4bae31bd33726b43
Gokulan <Gokulan@GokulanPC>
2018-10-09 01:40:41 +0530

    City Multiple filtering HotelCities CAPI

---
+++ src/main/java/com/hotelmgmt/entity/HotelCities.java
@@ -0,0 +1,15 @@
+package com.hotelmgmt.entity;
+
+public class HotelCities {
+	
+	private String [] cities;
+
+	public String[] getCities() {
+		return cities;
+	}
+
+	public void setCities(String[] cities) {
+		this.cities = cities;
+	}
+
+}
